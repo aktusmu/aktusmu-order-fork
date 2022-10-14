c2d7e39c2bbb5977f1bdc0d7fe938cd86d00da1f
macbook <aymanedroussi.off@gmail.com>
2022-10-14 17:17:01 +0100

    first commit

---
+++ change_svrmgr.py
@@ -0,0 +1,24 @@
+from flask_scrypt import generate_random_salt, generate_password_hash, check_password_hash
+
+def generate(userid, password):
+    credentials = userid + password
+    salt = generate_random_salt(64)
+    salt = salt.decode('utf-8')
+    hashed_credentials = generate_password_hash(credentials,salt)
+
+
+
+    hashed_credentials = hashed_credentials.decode('utf-8')
+
+    lines = [salt, hashed_credentials]
+    with open("manage.txt", "w") as file:
+        for line in lines:
+            file.write(line)
+            file.write('\n')
+        file.close()
+
+
+userid = input('UserID: ')
+password = input('Password: ')
+
+generate(userid,password)
